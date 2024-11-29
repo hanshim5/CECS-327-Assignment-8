90783588d378fe24b6c7f4af6d39f08b08a97df9
iselatamayo <iselat5862@gmail.com>
2024-11-29 10:39:33 -0800

    mongo connnection

--- Echo Server - Modified.py
+++ Echo Server - Modified.py
@@ -5,9 +5,6 @@
 # 172.20.10.2
 # 1026
 
-# 1. Receives the message from the client.
-# 2. Change the letters of the message to "capital letters" and send it back to the client by using the same socket.
-# 3. The server should be able to send multiple messages to the client. You may need to consider using the infinite loop as we discussed in the class.
 
 import socket
 from pymongo.mongo_client import MongoClient
