9967869f007557525ac3495fd30c61cc56a47d3e
iselatamayo <iselat5862@gmail.com>
2024-12-03 21:42:46 -0800

    integrated BST into queries

--- Echo Server - Modified.py
+++ Echo Server - Modified.py
@@ -92,6 +92,7 @@ class BST:
         return left_sum + right_sum + node.value, left_count + right_count + 1
 
 
+
 # Creating a TCP socket
 myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -134,18 +135,16 @@ while True:
                         "$lte": int(current_pst.timestamp() * 1000)}
                 }, {"_id": 0, "payload.Moisture Meter - Moisture Meter - Fridge": 1}) #only get sensor data
 
-                moisture_readings = [
-                    float(doc["payload"]["Moisture Meter - Moisture Meter - Fridge"]) for doc in fridge_data
-                ]
+                moisture_readings = BST()
+                for doc in fridge_data:
+                    moisture_readings.insert(float(doc["payload"]["Moisture Meter - Moisture Meter - Fridge"]))
 
-
-                if moisture_readings:
-                    avg_moisture = sum(moisture_readings) / len(moisture_readings)
+                avg_moisture = moisture_readings.calculate_average()
+                if avg_moisture == 0:
+                    response = "No data generated in past 3 hours."
+                else:
                     avg_rh = (avg_moisture / 40) * 100
                     response = f"Average moisture (RH%) in the last 3 hours: {avg_rh:.2f}%"
-                else:
-                    response = "No data found for the last 3 hours."
-
                 incomingSocket.send(response.encode('utf-8'))
 
 
@@ -154,16 +153,17 @@ while True:
                     "payload.parent_asset_uid": "ddo-08g-5f4-jsp"
                 }, {"_id": 0, "payload.Water Consumption Sensor - Dishwasher": 1}) #only get sensor data
 
-                water_readings = [
-                    float(doc["payload"]["Water Consumption Sensor - Dishwasher"]) for doc in dishwasher_data
-                ]
 
-                if water_readings:
-                    avg_cons = sum(water_readings)/len(water_readings)
-                    response = f"Average Water Consumption (in gallons) per cycle: {avg_cons:.2f}"
-                else:
+                water_readings = BST()
+                for doc in dishwasher_data:
+                    water_readings.insert(float(doc["payload"]["Water Consumption Sensor - Dishwasher"]))
+
+
+                avg_cons = water_readings.calculate_average()
+                if avg_cons == 0:
                     response = "No data found for water consumption sensor."
-
+                else:
+                    response = f"Average Water Consumption (in gallons) per cycle: {avg_cons:.2f}"
                 incomingSocket.send(response.encode('utf-8'))
 
             elif myData == "3": #which device consumed most electricity
@@ -182,28 +182,32 @@ while True:
 
 
                 #store data for all devices
-                fridge_1_readings = []  # collects only one sensor value per hour
+                fridge_1_readings = BST() #holds one sensor value per hour
                 unique1_hours = []
                 for doc in fridge_1:
                     time = doc["time"]
                     hour = datetime(time.year, time.month, time.day, time.hour)
                     if hour not in unique1_hours:
                         unique1_hours.append(hour)
-                        fridge_1_readings.append(float(doc["payload"]["Ammeter - Fridge"]))
+                        #adds only one sensor value per hour
+                        fridge_1_readings.insert(float(doc["payload"]["Ammeter - Fridge"]))
 
 
-                dishwasher_readings = [
-                    float(doc["payload"]["Ammeter - Dishwasher"]) for doc in dishwasher_data
-                ]
+                dishwasher_readings = BST()
+                dish_hours = 0
+                for doc in dishwasher_data:
+                    dishwasher_readings.insert(float(doc["payload"]["Ammeter - Dishwasher"]))
+                    dish_hours += 1   #assuming each data entry counts as 1 hour
 
-                fridge_2_readings = []  # collects only one sensor value per hour
+                fridge_2_readings = BST() #holds one sensor value per hour
                 unique2_hours = []
                 for doc in fridge_2:
                     time = doc["time"]
                     hour = datetime(time.year, time.month, time.day, time.hour)
                     if hour not in unique2_hours:
                         unique2_hours.append(hour)
-                        fridge_2_readings.append(float(doc["payload"]["sensor 2 a087ce6e-4be6-4a0b-ba16-8ad7016af76a"]))
+                        # adds only one sensor value per hour
+                        fridge_2_readings.insert(float(doc["payload"]["sensor 2 a087ce6e-4be6-4a0b-ba16-8ad7016af76a"]))
 
 
                 #CONVERSIONS : kWh = Amps x Volts x Hours / 1000
@@ -211,27 +215,17 @@ while True:
                 #dishwasher: assuming 120 volts
                 #dishwasher: assuming 1 cycle = 1 hour
                 #dishwasher readings currently in Amperes
-                if dishwasher_readings:
-                    dish_avg_amps = sum(dishwasher_readings)/len(dishwasher_readings)
-                else:
-                    dish_avg_amps = 0
-                dish_kWh = (dish_avg_amps * 120 * len(dishwasher_readings))/1000
+                dish_avg_amps = dishwasher_readings.calculate_average()
+                dish_kWh = (dish_avg_amps * 120 * dish_hours)/1000
 
                 #fridge: assuming 120 volts
                 #fridge: readings currently in Amperes
-
                 #Smart Fridge 1
-                if fridge_1_readings:
-                    fr1_avg_amps = sum(fridge_1_readings) / len(fridge_1_readings)
-                else:
-                    fr1_avg_amps = 0
+                fr1_avg_amps = fridge_1_readings.calculate_average()
                 fr1_kWh = (fr1_avg_amps * 120 * len(unique1_hours)) / 1000
 
                 #Smart Fridge 2
-                if fridge_2_readings:
-                    fr2_avg_amps = sum(fridge_2_readings) / len(fridge_2_readings)
-                else:
-                    fr2_avg_amps = 0
+                fr2_avg_amps = fridge_2_readings.calculate_average()
                 fr2_kWh = (fr2_avg_amps * 120 * len(unique2_hours))/1000
 
                 max_kWh = max(dish_kWh, fr1_kWh, fr2_kWh)
@@ -244,7 +238,7 @@ while True:
                     devices.append("Smart Dishwasher")
 
 
-                response = f"Max kWh consumed: {max_kWh} by following devices: {devices}"
+                response = f"Max kWh consumed: {max_kWh:.2f} by following devices: {devices}"
                 incomingSocket.send(response.encode('utf-8'))
 
             else:
